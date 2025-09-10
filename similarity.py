2fc003c090d56ebb879fdb8b3cc88b068e3d0c5c
EdgarO27 <ortizedgar092@gmail.com>
2025-09-10 14:30:29 -0700

    Completed Seperation of Strings with Split Method

--- similarity.py
+++ similarity.py
@@ -1,9 +1,9 @@
 # -------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Edgar Ortiz
+# FILENAME: Similiar Documents
+# SPECIFICATION: Find the most Similar documents based on cosine between vectors
 # FOR: CS 5990 (Advanced Data Mining) - Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: current 5 hours
 # -----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy or pandas.
@@ -25,36 +25,51 @@ with open('cleaned_documents.csv', 'r') as csvfile:
 #Building the document-term matrix by using binary encoding.
 #You must identify each distinct word in the collection using the white space as your character delimiter.
 #--> add your Python code here
+
 docTermMatrix = []
 
+# for singleDoc in documents:
+#   for words in singleDoc[1:2]:
+#     for i in words.split(' '):
+#       if i not in docTermMatrix:
+#         docTermMatrix.append([i])
 
-single = documents[0]
+#   #Iterate through documents one by one 
 
-# print(single)
+for singleDocument in documents[0:1]:
+  wordList = []
+#   #Iterate through all words skip the first index of document
+#   #since document has a size of 2
+#   #Outline :    ['1', 'word1 word2 word3 word4 word5']
+  for letters in singleDocument[1:]:
+    #   # We are iterating through the index of words 
+    #   # So now we have to create each entry based on the size of a specific document
+    #   #Make a word list that will add words 
+    #   #
+    for i in letters.split(' '):
+        wordList.append(i)
+
+      
+        # if i not in docTermMatrix:
+        #   docTermMatrix.append([0])
+        # else:
+        #    docTermMatrix.append(1)
+
+
+for singleDocument in documents
+    
+    
+
+print(wordList[0])
 
 
 
-     
-print(len(single[1]))
-# print( len(docTermMatrix))
 
 
-'''
-Approach:
 
-First Try:
-    1. Iterate through the documents and gather all words that are distinct ? 
 
-    2. then iterate the same way but if word is in the other list then we add a 1 to that specific space
-'''
 
-for singleDoc in documents:
-  for words in singleDoc[1:2]:
-    for i in words.split(' '):
-      if i not in docTermMatrix:
-        docTermMatrix.append([i])
 
-print(docTermMatrix[0])
 # Compare the pairwise cosine similarities and store the highest one
 # Use cosine_similarity([X], [Y]) to calculate the similarities between 2 vectors
 # --> Add your Python code here
