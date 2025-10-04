79931e1921e2a0e5cf4eb0ebc8b51420f2e40e7e
EdgarO27 <ortizedgar092@gmail.com>
2025-10-03 17:26:54 -0700

    Adding final touches

--- Hw2/pca.py
+++ Hw2/pca.py
@@ -25,14 +25,16 @@ scaled_data = scaler.fit_transform(df)
         #KEY IN EXTRACTING SPECIFIC COLUMNS AND ITS DATA 
 num_features = len(df.columns)
 pca_variance = []
+pca_var = {}
 # Run PCA using 9 features, removing one feature at each iteration
-for i in range(3):
+for i in range(num_features):
 #     # Create a new dataset by dropping the i-th feature
     reduced_data = df.drop(columns=[df.columns[i]])
     
 #     # Run PCA on the reduced dataset
 #         # WE USE LEN SINCE REDUCED DATA HAS ONE COLUMN LESS THAN THE ORIGINAL DATA 
-    pca = PCA(n_components=2)
+# subtract one from num of features since every iteration has one col less to test 
+    pca = PCA(n_components=num_features-1)
 
     X = pca.fit_transform(reduced_data)
     # print("This is the column that was dropped")
@@ -44,17 +46,24 @@ for i in range(3):
     
 #     print(pca.explained_variance_ratio_)
 #     # pca_variance.append()
-    pca_var = {}
-    pca_var[df.c]
+    print(pca.explained_variance_ratio_)
+    pca_var[df.columns[i]] = np.max(pca.explained_variance_ratio_)
 #     #Use pca.explained_variance_ratio_[0] and df_features.columns[i] for that
 #     # --> add your Python code here
-    pca_variance.append([df.columns[i], pca.explained_variance_ratio_[0]])
+    # pca_variance.append([df.columns[i], pca.explained_variance_ratio_[0]])
+max_pca = pca_var[0] #use the first item to compare
+feature ='' # attach the name 
+for index, data in enumerate(pca_var):
 
+    if pca_var[i] > max_pca:
 
+        max_pca = pca_var[i]
+    
+    
 # Find the maximum PC1 variance
 # --> add your Python code here
 
-print(pca_variance)
+print(max_pca)
 #Print results
 #Use the format: Highest PC1 variance found: ? when removing ?
 
