4b902e738214a4583caed17eea051c8b57b8fefb
edgaro9 <edgaro@cpp.edu>
2025-11-06 00:27:26 -0800

    Forgot to push code

--- hw3/decision_tree.py
+++ hw3/decision_tree.py
@@ -13,55 +13,106 @@ from sklearn import tree
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.metrics import accuracy_score
 
-dataSets = ['cheat_training_1.csv', 'cheat_training_2.csv', 'cheat_training_3.csv']
+
+#  'cheat_training_2.csv', 'cheat_training_3.csv'
+dataSets = ['cheat_training_1.csv']
 
 for ds in dataSets:
 
     X = []
     Y = []
 
+
+
     df = pd.read_csv(ds, sep=',', header=0)   #reading a dataset eliminating the header (Pandas library)
+    
+    
     data_training = np.array(df.values)[:,1:] #creating a training matrix without the id (NumPy library)
 
     #transform the original training features to numbers and add them to the 5D array X. For instance, Refund = 1, Single = 1, Divorced = 0, Married = 0,
+    refund_encoded = np.where(data_training[:, 0] == 'Yes', 1, 0)
+    marital_status = data_training[:, 1]
+    single = (marital_status == 'Single').astype(int)
+    divorced = (marital_status == 'Divorced').astype(int)
+    married = (marital_status == 'Married').astype(int)
+    income_clean = np.char.replace(data_training[:, 2].astype(str), 'k', '')
+    income_floats = income_clean.astype(float)
+    labels_encoded = np.where(data_training[:, 3] == 'Yes', 1, 2)
+
+    X = np.column_stack((refund_encoded, single, divorced, married, income_floats)).tolist()
+    Y = labels_encoded.tolist()
+
+
+
+   
+    
+
+    
     #Taxable Income = 125, so X = [[1, 1, 0, 0, 125], [2, 0, 1, 0, 100], ...]]. The feature Marital Status must be one-hot-encoded and Taxable Income must
     #be converted to a float.
     # X =
-
+    X = np.array(X)
     #transform the original training classes to numbers and add them to the vector Y. For instance Yes = 1, No = 2, so Y = [1, 1, 2, 2, ...]
     #--> add your Python code here
     # Y =
-
+    Y = np.array(Y)
+    accuracy = []
     #loop your training and test tasks 10 times here
     for i in range (10):
 
-       #fitting the decision tree to the data by using Gini index and no max_depth
-       clf = tree.DecisionTreeClassifier(criterion = 'gini', max_depth=None)
-       clf = clf.fit(X, Y)
+    #    #fitting the decision tree to the data by using Gini index and no max_depth
+        clf = tree.DecisionTreeClassifier(criterion = 'gini', max_depth=None)
+        clf = clf.fit(X, Y)
 
-       #plotting the decision tree
-       tree.plot_tree(clf, feature_names=['Refund', 'Single', 'Divorced', 'Married', 'Taxable Income'], class_names=['Yes','No'], filled=True, rounded=True)
-       plt.show()
+    #    #plotting the decision tree
+        tree.plot_tree(clf, feature_names=['Refund', 'Single', 'Divorced', 'Married', 'Taxable Income'],     class_names=['Yes','No'], filled=True, rounded=True)
+        plt.show()
 
-       #read the test data and add this data to data_test NumPy
-       #--> add your Python code here
-       # data_test =
+    #    #read the test data and add this data to data_test NumPy
+    #    #--> add your Python code here
+        df_test = pd.read_csv('cheat_test.csv', sep=',', header=0)
+        data_test = np.array(df_test.values)[:,1:]
 
-       for data in data_test:
-           #transform the features of the test instances to numbers following the same strategy done during training, and then use the decision tree to make the class prediction. For instance:
-           #class_predicted = clf.predict([[1, 0, 1, 0, 115]])[0], where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
-           #--> add your Python code here
 
-           #compare the prediction with the true label (located at data[3]) of the test instance to start calculating the model accuracy.
-           #--> add your Python code here
 
-       #find the average accuracy of this model during the 10 runs (training and test set)
-       #--> add your Python code here
+        for data in data_test:
+    #        #transform the features of the test instances to numbers following the same strategy done during training, and then use the decision tree to make the class prediction. For instance:
+    #        #class_predicted = clf.predict([[1, 0, 1, 0, 115]])[0], where [0] is used to get an integer as the predicted class label so that you can compare it with the true label
+    #        #--> add your Python code here
+            refund_encoded = np.where(data_test[:, 0] == 'Yes', 1, 2)
+            marital_status = data_test[:, 1]
+            single = (marital_status == 'Single').astype(int)
+            divorced = (marital_status == 'Divorced').astype(int)
+            married = (marital_status == 'Married').astype(int)
+            income_clean = np.char.replace(data_test[:, 2].astype(str), 'k', '')
+            income_floats = income_clean.astype(float)
+            true_labels = np.where(data_test[:, 3] == 'Yes', 1, 2)
 
-    #print the accuracy of this model during the 10 runs (training and test set).
-    #your output should be something like that: final accuracy when training on cheat_training_1.csv: 0.2
-    #--> add your Python code here
+
+            X_test = np.column_stack((refund_encoded, single, divorced, married, income_floats))
+
+
+
+            predicted_labels = clf.predict(X_test)
+
+
+
+        correct = np.sum(predicted_labels == true_labels)
+        total = len(true_labels)
+        acc = correct / total
+        accuracy.append(acc)
+
+
+
+
+    final_accuracy = sum(accuracy) / len(accuracy)
+    print(f"final accuracy when training on {ds}: {final_accuracy:.2f}")
+
+
 
 
 
--- hw3/roc_curve.py
+++ hw3/roc_curve.py
@@ -19,59 +19,73 @@ import pandas as pd
 
 # read the dataset cheat_data.csv and prepare the data_training numpy array
 # --> add your Python code here
-# data_training = ?
-
+df = pd.read_csv('cheat_data.csv', sep=',', header=0)   #reading a dataset eliminating the header (Pandas library)
+    
+    
+data_training = np.array(df.values) #creating a training matrix without the id (NumPy library)
 # transform the original training features to numbers and add them to the 5D array X. For instance, Refund = 1, Single = 1, Divorced = 0, Married = 0,
 # Taxable Income = 125, so X = [[1, 1, 0, 0, 125], [0, 0, 1, 0, 100], ...]]. The feature Marital Status must be one-hot-encoded and Taxable Income must
 # be converted to a float.
 # --> add your Python code here
-# X = ?
 
-# transform the original training classes to numbers and add them to the vector y. For instance Yes = 1, No = 0, so Y = [1, 1, 0, 0, ...]
-# --> add your Python code here
-# y = ?
+refund_encoded = np.where(data_training[:, 0] == 'Yes', 1, 0)
+marital_status = data_training[:, 1]
+single = (marital_status == 'Single').astype(int)
+divorced = (marital_status == 'Divorced').astype(int)
+married = (marital_status == 'Married').astype(int)
+income_clean = np.char.replace(data_training[:, 2].astype(str), 'k', '')
+income_floats = income_clean.astype(float)
+labels_encoded = np.where(data_training[:, 3] == 'Yes', 0, 1)
+
+X = np.column_stack((refund_encoded, single, divorced, married, income_floats)).tolist()
+y = labels_encoded.tolist()
+
+
+
+
 
 # split into train/test sets using 30% for test
 # --> add your Python code here
-trainX, testX, trainy, testy = train_test_split(X, y, test_size = ?)
+trainX, testX, trainy, testy = train_test_split(X, y, test_size = 0.3)
 
 # generate random thresholds for a no-skill prediction (random classifier)
 # --> add your Python code here
-# ns_probs = ?
+ns_probs = np.random.rand(len(testy))
 
 # fit a decision tree model by using entropy with max depth = 2
- clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=?)
- clf = clf.fit(trainX, trainy)
+clf = tree.DecisionTreeClassifier(criterion = 'entropy', max_depth=2)
+clf = clf.fit(trainX, trainy)
 
 # predict probabilities for all test samples (scores)
- dt_probs = clf.predict_proba(testX)
+dt_probs = clf.predict_proba(testX)
 
 # keep probabilities for the positive outcome only
 # --> add your Python code here
-# dt_probs = ?
+dt_probs = clf.predict_proba(testX)[:, 1]
+
 
 # calculate scores by using both classifiers (no skilled and decision tree)
- ns_auc = roc_auc_score(testy, ns_probs)
- dt_auc = roc_auc_score(testy, dt_probs)
+ns_auc = roc_auc_score(testy, ns_probs)
+dt_auc = roc_auc_score(testy, dt_probs)
 
 # summarize scores
- print('No Skill: ROC AUC=%.3f' % (ns_auc))
- print('Decision Tree: ROC AUC=%.3f' % (dt_auc))
+print('No Skill: ROC AUC=%.3f' % (ns_auc))
+print('Decision Tree: ROC AUC=%.3f' % (dt_auc))
 
 # calculate roc curves
- ns_fpr, ns_tpr, _ = roc_curve(testy, ns_probs)
- dt_fpr, dt_tpr, _ = roc_curve(testy, dt_probs)
+ns_fpr, ns_tpr, _ = roc_curve(testy, ns_probs)
+dt_fpr, dt_tpr, _ = roc_curve(testy, dt_probs)
 
 # plot the roc curve for the model
- pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
- pyplot.plot(dt_fpr, dt_tpr, marker='.', label='Decision Tree')
+pyplot.plot(ns_fpr, ns_tpr, linestyle='--', label='No Skill')
+pyplot.plot(dt_fpr, dt_tpr, marker='.', label='Decision Tree')
 
 # axis labels
- pyplot.xlabel('False Positive Rate')
- pyplot.ylabel('True Positive Rate')
+pyplot.xlabel('False Positive Rate')
+pyplot.ylabel('True Positive Rate')
 
 # show the legend
- pyplot.legend()
+pyplot.legend()
 
 # show the plot
- pyplot.show()
+pyplot.show()
